afaefc5841317f188be13c53369aaa99961e993f
Chadthida <645051200014@mail.rmutk.ac.th>
2024-04-11 16:48:55 +0700

    youtube

--- app/page.js
+++ app/page.js
@@ -2,112 +2,311 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <body style={{ backgroundColor: "black", color: "white" }}>
+      <table border={0} cellSpacing={0}>
+        <tr>
+          <td width={40}>
+            <button class="moreoptions"></button>
+          </td>
+          <td width={140}>
+            <button class="youtube"></button>
+          </td>
+          <td width={830}>
+            <input type="text" class="searchinput" placeholder="Search" style={{ marginLeft: "290px" }} /><button class="search" style={{ verticalAlign: "middle" }}></button>
+          </td>
+          <td width={300}>
+            <button class="mic"></button>
+          </td>
+          <td>
+            <button class="create"></button>
+          </td>
+          <td>
+            <button class="notification"></button>
+          </td>
+          <td>
+            <button class="profile"></button>
+          </td>
+        </tr>
+      </table>
+      <table border={0} cellSpacing={0}>
+        <tr>
+          <td>
+            <table border={0} cellSpacing={0} frame="void" rules="rows">
+              <tr>
+                <table border={0} cellSpacing={0}>
+                  <tr>
+                    <td width={40}>
+                      <button class="home"></button>
+                    </td>
+                    <td width={140}>
+                      Home
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <button class="shorts"></button>
+                    </td>
+                    <td>
+                      Shorts
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <button class="subscribe"></button>
+                    </td>
+                    <td>
+                      Subscription
+                    </td>
+                  </tr>
+                </table>
+              </tr>
+              <tr>
+                <td>
+                  <button class="rightarrow"><b>You</b></button>
+                  <table border={0} cellSpacing={0}>
+                    <tr>
+                      <td width={40}>
+                        <button class="history"></button>
+                      </td>
+                      <td width={140}>
+                        History
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <button class="watchlater"></button>
+                      </td>
+                      <td>
+                        Watch later
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <button class="like"></button>
+                      </td>
+                      <td>
+                        Liked videos
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button class="title"><b>Explore</b></button>
+                  <table border={0} cellSpacing={0}>
+                    <tr>
+                      <td width={40}>
+                        <button class="trending2"></button>
+                      </td>
+                      <td width={140}>
+                        Trending
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <button class="music"></button>
+                      </td>
+                      <td>
+                        Music
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <button class="gaming"></button>
+                      </td>
+                      <td>
+                        Gaming
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <button class="news"></button>
+                      </td>
+                      <td>
+                        News
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <button class="sport"></button>
+                      </td>
+                      <td>
+                        Sport
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button class="title"><b>More from YouTube</b></button>
+                  <table border={0} cellSpacing={0}>
+                    <tr>
+                      <td width={40}>
+                        <button class="youtubepremium"></button>
+                      </td>
+                      <td width={140}>
+                        YouTube Premium
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <button class="youtubemusic"></button>
+                      </td>
+                      <td>
+                        YouTube Music
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <button class="youtubekids"></button>
+                      </td>
+                      <td>
+                        YouTube Kids
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+              <tr></tr>
+              <tr>
+                <table border={0} cellSpacing={0}>
+                  <tr>
+                    <td width={40}>
+                      <button class="setting"></button>
+                    </td>
+                    <td width={140}>
+                      Setting
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <button class="report"></button>
+                    </td>
+                    <td>
+                      Report history
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <button class="help"></button>
+                    </td>
+                    <td>
+                      Help
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <button class="comment"></button>
+                    </td>
+                    <td>
+                      Send feedback
+                    </td>
+                  </tr>
+                </table>
+              </tr>
+            </table>
+          </td>
+          <td style={{ verticalAlign: "top" }}>
+            <table border={0} cellSpacing={0}>
+              <tr>
+                <td width={150}></td>
+                <td>
+                  <button class="trending1"><b>Trending</b></button>
+                </td>
+              </tr>
+            </table>
+            <table border={0} cellSpacing={0} frame="void" rules="rows">
+              <tr>
+                <td width={150} height={45}></td>
+                <td width={50} align="center" class="selected">Now</td>
+                <td width={60} align="center" style={{color:"#b2b2b2"}}>Music</td>
+                <td width={70} align="center" style={{color:"#b2b2b2"}}>Gaming</td>
+                <td width={80} align="center" style={{color:"#b2b2b2"}}>Movies</td>
+                <td width={426}></td>
+                <td width={406}></td>
+              </tr>
+              <tr></tr>
+            </table>
+            <table border={0} cellSpacing={0}>
+              <tr style={{ verticalAlign: "top" }}>
+                <td width={150} height={160}></td>
+                <td style={{verticalAlign:"middle"}}><img src="/images/clip1.png" width={240} /></td>
+                <td width={460}>
+                  <p style={{ fontSize: "13px" }}>
+                    การแข่งขัน RoV Pro League 2024 Summer | รอบ Regular Season วัน<br />สุดท้าย
+                  </p>
+                  <p style={{ fontSize: "9.5px", color: "#b2b2b2" }}>
+                    Garena Rov Thailand <img src="/images/verify.png" width={9} /> 1M views・Streamed 20 hours ago
+                  </p>
+                  <p style={{ fontSize: "9px", color: "#b2b2b2" }}>
+                    การแข่งขัน Rov Pro League 2024 Summer🏆 วันสุดท้ายของรอบ Regular Season(เก็บคะแนน)🏆งานออฟไลน์<br />รอ...
+                  </p>
+                </td>
+              </tr>
+              <tr style={{ verticalAlign: "top" }}>
+                <td width={150} height={160}></td>
+                <td style={{verticalAlign:"middle"}}><img src="/images/clip2.png" width={240} /></td>
+                <td width={455}>
+                  <p style={{ fontSize: "13px" }}>
+                    สวยขยี้ใจ - บ่าวบุ๊ค x ทิดแอม x คำมอส #อัลบั้มละไว้ในฐานที่เข้าใจ<br /> [OFFICIAL MV]
+                  </p>
+                  <p style={{ fontSize: "9.5px", color: "#b2b2b2" }}>
+                    บังเอิญ มิวสิค・3.6M views・3 days ago
+                  </p>
+                  <p style={{ fontSize: "9px", color: "#b2b2b2" }}>
+                    เพลง : สวยขยี้ใจ ศิลปิน : บุ๊ค ศุภกาญจน์,ทิดแอม,มอส คำหมากบิน คำร้อง/ทำนอง...
+                  </p>
+                </td>
+              </tr>
+              <tr style={{ verticalAlign: "top" }}>
+                <td width={150} height={160}></td>
+                <td style={{verticalAlign:"middle"}}><img src="/images/clip3.png" width={240} /></td>
+                <td width={455}>
+                  <p style={{ fontSize: "13px" }}>
+                    ผม vs แฟนตอนไปทะเล 😂😂
+                  </p>
+                  <p style={{ fontSize: "9.5px", color: "#b2b2b2" }}>
+                    Eoirndaime <img src="/images/verify.png" width={9} /> 498K views・2 days ago
+                  </p>
+                </td>
+              </tr>
+              <tr style={{ verticalAlign: "top" }}>
+                <td width={150} height={160}></td>
+                <td style={{verticalAlign:"middle"}}><img src="/images/clip4.png" width={240} /></td>
+                <td width={455}>
+                  <p style={{ fontSize: "13px" }}>
+                    Girl Are Strong Too 💪
+                  </p>
+                  <p style={{ fontSize: "9.5px", color: "#b2b2b2" }}>
+                    Alan Chikin Chow <img src="/images/verify.png" width={9} /> 4.2 views・22 hours ago
+                  </p>
+                  <p style={{ fontSize: "8.5px", color: "#b2b2b2" }}>
+                    Hey Heroes, this is Alan Chikin Chow! Thanks for watching my video 😍 #Shorts 📩CONNECT WITH ME: IG:<br />
+                    https://www.instagram.com/alanchikinchow EMAIL:alan@alanchikinchow.com
+                  </p>
+                </td>
+              </tr>
+              <tr style={{ verticalAlign: "top" }}>
+                <td width={150} height={160}></td>
+                <td style={{verticalAlign:"middle"}}><img src="/images/clip5.png" width={240} /></td>
+                <td width={455}>
+                  <p style={{ fontSize: "13px" }}>
+                    ฟังสดเดอะโกสเรดิโอ 24/3/2567 เรื่องเล่าผีเดอะโกส
+                  </p>
+                  <p style={{ fontSize: "9.5px", color: "#b2b2b2" }}>
+                    TheghostradioOfficial <img src="/images/verify.png" width={9} /> 495K views・Streamed 11 hours ago
+                  </p>
+                  <p style={{ fontSize: "9px", color: "#b2b2b2" }}>
+                    00:00:00 เปิดรายการ 28:49 สายที่ 1 คุณอ๊อด - ผีริมคลอง 50:45 สายที่ 2 คุณเอ - ผีห่าก้อมลิ...
+                  </p>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </body>
   );
 }
